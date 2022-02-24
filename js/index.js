1aa1c462fe123b6f055d9af53ec82e82905932d3
Sander Vonk <sander.c.vonk@gmail.com>
2022-02-24 11:20:27 -0800

    add basic layout and page functionality

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 $(".nav-bar-item").click((e) => {
     $("#nav-bar, #nav-page-carousel").attr("class", "active-" + e.target.id)
 })
+$(".footer-segment").click((e) => {
+    $("footer, #page-container").attr("class", "active-" + e.target.id)
+})

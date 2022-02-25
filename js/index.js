ac35940fc9a1a25592efe04faf56e4c0457e6c7c
Sander Vonk <sander.c.vonk@gmail.com>
2022-02-24 17:20:01 -0800

    simple prototype for the timer page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
 $(".nav-bar-item").click((e) => {
     $("#nav-bar, #nav-page-carousel").attr("class", "active-" + e.target.id)
+    $(".nav-page").removeClass("active")
+    $("#nav-page-carousel > #" + e.target.id).addClass("active")
 })
 $(".footer-segment").click((e) => {
     $("footer, #page-container").attr("class", "active-" + e.target.id)
+})
+
+$(".round").click((e) => {
+    if (e.target.offsetTop != $(".nav-page.active .round.active")[0].offsetTop) {
+        if (e.target.offsetTop < $(".nav-page.active .round.active")[0].offsetTop) {
+            $(e.target.parentElement.parentElement.getElementsByClassName("timer-highlight")).css({
+                "top": e.target.offsetTop - e.target.parentElement.parentElement.offsetTop + 15
+            })
+            console.log("true")
+        } else {
+            $(e.target.parentElement.parentElement.getElementsByClassName("timer-highlight")).css({
+                "top": e.target.offsetTop - e.target.parentElement.parentElement.offsetTop + 15 - 56
+            })
+        }
+    }
+    $(".nav-page.active .round.active").removeClass("active")
+    $(e.target).addClass("active")
 })

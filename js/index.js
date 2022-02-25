8ed0e75bca4dd89ed84b0ec21af54f2f9efb408f
Sander Vonk <sander.c.vonk@gmail.com>
2022-02-24 18:38:12 -0800

    basic timer features

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
+function msToTime(s) {
+
+    // Pad to 2 or 3 digits, default is 2
+    function pad(n, z) {
+        z = z || 2;
+        return ('00' + n).slice(-z);
+    }
+
+    var ms = s % 1000;
+    s = (s - ms) / 1000;
+    var secs = s % 60;
+    s = (s - secs) / 60;
+    var mins = s % 60;
+    var hrs = (s - mins) / 60;
+
+    return mins + ':' + pad(secs);
+}
+var timerData;
 $(".nav-bar-item").click((e) => {
     $("#nav-bar, #nav-page-carousel").attr("class", "active-" + e.target.id)
     $(".nav-page").removeClass("active")
@@ -6,20 +24,83 @@ $(".nav-bar-item").click((e) => {
 $(".footer-segment").click((e) => {
     $("footer, #page-container").attr("class", "active-" + e.target.id)
 })
+class timer {
 
+
+    start(parent, length) {
+        this.status = "running";
+        this.clear()
+        this.parent = parent;
+        this.length = length;
+        this.startTime = (new Date).getTime();
+        this.update()
+        this.id = window.setInterval(function () {
+            timerData.update()
+        }, 500)
+    }
+    clear() {
+        this.stop()
+        this.time = 0;
+        $(this.parent).find(".timer-bar").css({
+            "width": "0%"
+        })
+        $(this.parent).find(".timer-timestamp").text("0:00")
+    }
+    stop() {
+        this.status = "";
+        console.log(this.id)
+        window.clearInterval(this.id)
+    }
+    update() {
+        this.status = "running";
+        this.time = (new Date).getTime() - this.startTime;
+        let width = 100 * this.time / this.length;
+        width += "%"
+        $(this.parent).find(".timer-bar").css({
+            "width": width
+        })
+        $(this.parent).find(".timer-timestamp").text(msToTime(this.time));
+        if (this.time >= this.length) {
+            this.stop()
+        }
+    }
+
+}
+
+var timerData = new timer();
 $(".round").click((e) => {
     if (e.target.offsetTop != $(".nav-page.active .round.active")[0].offsetTop) {
         if (e.target.offsetTop < $(".nav-page.active .round.active")[0].offsetTop) {
             $(e.target.parentElement.parentElement.getElementsByClassName("timer-highlight")).css({
                 "top": e.target.offsetTop - e.target.parentElement.parentElement.offsetTop + 15
             })
-            console.log("true")
         } else {
             $(e.target.parentElement.parentElement.getElementsByClassName("timer-highlight")).css({
                 "top": e.target.offsetTop - e.target.parentElement.parentElement.offsetTop + 15 - 56
             })
         }
     }
+    try {
+        timerData.clear()
+    } catch {
+        console.log("could not clear timer")
+    }
     $(".nav-page.active .round.active").removeClass("active")
     $(e.target).addClass("active")
+})
+$("#timers-FAB, #timer-pause").click(function () {
+    //timer functionality here
+    if (timerData.status == "running") {
+        timerData.stop()
+    } else {
+        timerData.start($(".nav-page.active")[0], parseInt($(".nav-page.active .round.active .round-time").text()) * 60000);
+
+    }
+
+})
+
+$("#timer-reset").click(function () {
+    try {
+        timerData.clear()
+    } catch {}
 })
